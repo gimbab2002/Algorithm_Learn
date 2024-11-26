b63f05c7fe7a7919dd6668127bd84863c6a99b3a
kimdongkyun <69901143+gimbab2002@users.noreply.github.com>
2024-11-26 16:31:30 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    scanf("%d %d",&a, &b);
+    if(a<10, b<10, a>0, b>0){
+        printf("%d",a+b);
+    }else{
+        return 0;
+    }
+    
+    return 0;
+}
