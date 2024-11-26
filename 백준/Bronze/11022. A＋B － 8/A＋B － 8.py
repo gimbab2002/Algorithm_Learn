d9dcdba4f52d71e0bb0f1880efa201239cf7130a
kimdongkyun <69901143+gimbab2002@users.noreply.github.com>
2024-11-26 16:52:28 +0900

    [Bronze V] Title: A+B - 8, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,8 @@
+n = int(input())
+a, b = [], []
+for i in range(n):
+    p, q = map(int, input().split())
+    a.append(p)
+    b.append(q)
+for i in range(n):
+    print(f"Case #{i+1}: {a[i]} + {b[i]} = {a[i] + b[i]}")
