7f89096b4ceb28c5563a6af01d6116e5a3c1b0d8
kimdongkyun <69901143+gimbab2002@users.noreply.github.com>
2025-06-28 01:20:25 +0900

    [Bronze II] Title: 음계, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+ascending = [1,2,3,4,5,6,7,8]
+descending = [8,7,6,5,4,3,2,1]
+a=list(map(int, input().rstrip().split()))
+if a == ascending:
+    print('ascending')
+elif a == descending:
+    print('descending')
+else: print('mixed')
