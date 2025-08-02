538bf85b695d9726a781722b3edcedc9d386a6c9
kimdongkyun <69901143+gimbab2002@users.noreply.github.com>
2025-08-03 04:15:51 +0900

    [Bronze IV] Title: ISBN, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/6810. ISBN/ISBN.py
@@ -0,0 +1,11 @@
+isbn = "9780921418"
+for _ in range(3):
+    isbn += input()
+
+res = 0
+for idx, i in enumerate(isbn):
+    if idx % 2 == 1:
+        res += int(i)*3
+    else:
+        res += int(i)
+print(f"The 1-3-sum is {res}")
